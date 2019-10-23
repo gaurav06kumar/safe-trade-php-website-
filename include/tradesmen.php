d47f1d2da5ac27a6c0f6014b24e9d14d8de05b35
gaurav <gauravkumar5823@gmail.com>
2019-10-23 22:58:42 +1300

    Update tradesmen.php
    add property

--- include/tradesmen.php
+++ include/tradesmen.php
@@ -1,21 +1,32 @@
 <?php
 	require_once("db_config.php");
 	class tradesmen{
-		protected $db;
-		public function __construct(){
-			$this->db = new DB_con();
-			$this->db = $this->db->ret_obj();
-		}
+  public $tid =null;
+  public $tname ="";
+	public $tusername="";
+	public $tpassword="";
+	public $temail="";
+
+
+
+
+	public function __construct($tid,$name,$username,$password,$email){
+ 	 $this->tid = $tid;
+ 	 $this->tname = $name;
+ 	 $this->tusername = $username;
+ 	 $this->tpassword = $password;
+ 	 $this->temail = $email;
+  }
 
 		/*** for registration process ***/
 
-		public function reg_user($name,$username,$password,$email){
+		public static function reg_user($db,$name,$username,$password,$email){
 			//echo "k";
 
 			//checking if the username or email is available in db
 			$query = "SELECT * FROM tradesmeninfo WHERE T_username='$username' OR T_email='$email'";
 
-			$result = $this->db->query($query) or die($this->db->error);
+			$result = $db->query($query) or die($db->error);
 
 			$count_row = $result->num_rows;
 
@@ -24,44 +35,54 @@
 			if($count_row == 0){
 				$query = "INSERT INTO tradesmeninfo SET T_username='$username', T_pass='$password', T_fname='$name', T_email='$email'";
 
-				$result = $this->db->query($query) or die($this->db->error);
+				$result = $db->query($query) or die($db->error);
+				if($result)
+				{
+					$tid = $db->insert_id;
+					$tuser = new tradesmen($tid,$name,$username,$password,$email);
+		      $result = $tuser;
+				}
+				 return $result;
 
-				return true;
+
 			}
-			else{return false;}
+			else{
+				$x='0';
+			return $x;}
 
 
 			}
 
 
 	/*** for login process ***/
-		public function check_login($emailusername, $password){
+		public static function check_login($db,$emailusername, $password){
       //  $password = md5($password);
 
-		$query = "SELECT TID from tradesmeninfo WHERE T_email='$emailusername'   and T_pass='$password'";
+		$query = "SELECT * from tradesmeninfo WHERE T_email='$emailusername'   and T_pass='$password'";
 
-		$result = $this->db->query($query) or die($this->db->error);
+		$result = $db->query($query) or die($db->error);
 
 
 		$user_data = $result->fetch_array(MYSQLI_ASSOC);
 		$count_row = $result->num_rows;
 
 		if ($count_row == 1) {
-
+			$user = new tradesmen($user_data['TID'],$user_data['T_fname'],$user_data['T_username'],$user_data['T_pass'],$user_data['T_email']);
+			$result = $user;
 	            $_SESSION['TID'] = $user_data['TID'];
-	            return true;
+	            return $result;
 	        }
 
-		else{return false;}
+
 
 
 	}
 
 
-	public function get_fullname($tid){
+	public static function get_fullname($db,$tid){
 		$query = "SELECT T_fname FROM tradesmeninfo WHERE TID = $tid";
 
-		$result = $this->db->query($query) or die($this->db->error);
+		$result = $db->query($query) or die($db->error);
 
 		$user_data = $result->fetch_array(MYSQLI_ASSOC);
 		echo $user_data['T_fname'];
