9c1cadbf083d017ac5df80bda45ced01a411b358
gaurav <gauravkumar5823@gmail.com>
2019-11-01 20:59:07 +1300

    Update estimate.php
    add getter method
    add cuatomer estimate function
    add find function
    add delete function
    add accept function

--- include/estimate.php
+++ include/estimate.php
@@ -101,7 +101,7 @@ public function __construct(  $eid, $jid, $tid, $lcost ,$tcost, $mcost, $totaltc
 						 <td><?= $res['Total_Cost']; ?> </td>
 				 
 						 <td><?= $res['Expiry_Date']; ?> </td>
-						 <td><a href="jobdetail.php?eid=<?php echo $res['eid']; ?>" class="btn btn-info">Delete</a></td>
+						 <td><a href="delestimate.php?eid=<?php echo $res['eid']; ?>" class="btn btn-info">Delete</a></td>
 
 						 </tr>
 				 </div>
@@ -120,59 +120,207 @@ public function __construct(  $eid, $jid, $tid, $lcost ,$tcost, $mcost, $totaltc
  
 
 
- public static function find($db, $id){
-   // search students table and locate record with id
-   // get that record and create student object
-   // return student object OR false if we cannot find it
-   $result = false;
-   $query = "SELECT * FROM job WHERE jid = $id";
-     $result = $db->query($query) or die($db->error);
-   if ($result){
-     if ($result->num_rows == 1){
-       $user_data = $result->fetch_assoc();
 
-         $user = new Job($user_data['job_name'],$user_data['location'],$user_data['Discription'],$user_data['Expected_Cost'],$user_data['Start_Date'],$user_data['End_Date'],$user_data['uid'],$user_data['jid']);
 
-       $result = $user;
-     }
-   }
-   return $result;
+
+ public static function view_c_estimate($db,$jid)
+ {  
+	$query ="SELECT * FROM `estimate` WHERE jid ='$jid' ";
+	 $result = $db->query($query) or die($db->error);
+ 
+
+		 ?>
+		 <table class = "table table-hover">
+		 <thead>
+		 <tr>
+
+		 
+	 
+		 
+		 <th>Labour Cost</th>
+		 <th>Material Cost</th>
+		 <th>Transport Cost</th>
+		 <th>Total Cost</th>
+		  <th> Expire Date</th>
+
+
+		 </tr>
+		 </thead>
+		 <tbody>
+		 <?php
+ 
+
+		 while($res = $result->fetch_assoc())
+		 {
+
+
+       
+			 if($res!=0)
+			 {
+				 ?>
+				 
+ 
+				 <div class="container-fluid">
+
+						 <tr>
+
+					 
+						  
+					 
+						 <td><?= $res['Labour_Cost']; ?> </td>
+						 <td><?= $res['Material_Cost']; ?> </td>
+						 <td><?= $res['Transport_Cost']; ?> </td>
+					 
+						 <td><?= $res['Total_Cost']; ?> </td>
+				 
+						 <td><?= $res['Expiry_Date']; ?> </td>
+						 <td><a href="accept_estimate.php?eid=<?php echo $res['eid']; ?>" class="btn btn-info">Accept</a></td>
+
+						 </tr>
+				 </div>
+
+		 <?php
+       }
+       else
+       {
+         return "Sorry No Records Found..";
+       }
+
+		 }?>
+		 </tbody>
+		 </table>
+		 <?php
  }
 
 
 
+
+ public static function find($db, $eid){
+	// search students table and locate record with id
+	// get that record and create student object
+	// return student object OR false if we cannot find it
+	$result = false;
+	$query = "SELECT * FROM estimate  WHERE eid = $eid";
+	  $result = $db->query($query) or die($db->error);
+	if ($result){
+	  if ($result->num_rows == 1){
+		$user_data = $result->fetch_assoc();
+
+	  
+		  $user = new estimate($user_data['eid'],$user_data['jid'],$user_data['tid'],$user_data['Labour_Cost'],$user_data['Transport_Cost'],$user_data['Material_Cost'],$user_data['Total_Cost'],$user_data['Expiry_Date'],$user_data['IsAccepted']);
+ 
+		$result = $user;
+	  }
+	}
+
+
+ 
+
+
+	return $result;
+  }
+ 
+
+ 
+
+ public static function delete($db,$eid){
+  //delete estimate record in estimatedetails table and if successful 
+  // return true otherwise return false;
+
+
+
+  $query ="SELECT * FROM `estimate` WHERE eid ='$eid' ";
+
+  $result = $db->query($query) or die($db->error);
+
+  if ($result){
+    if ($result->num_rows == 1){
+      $row = $result->fetch_assoc();
+      $_SESSION['TID']=$row['tid'];
+    }
+  }
+  $result = false;  //   $query ="DELETE FROM `estimate` WHERE eid = '$id'" ;
+  $query ="DELETE FROM `estimate` WHERE eid = '$eid'" ;
+  $result = $db->query($query) or die($db->error);
+ 
+
+  if ($result){
+   
+    $result = true;
+  }
+  return $result;
+}
+
+
+public static function accept($db,$eid){
+    // update estimate record in estimatedetails table and if user accepts the estimate 
+    // create a estimate object and return it otherwise return false;
+    $result = false;
+    // $tcost=$lcost+$mcost;
+	$isaccepted=1;
+	
+
+	$query ="SELECT * FROM `estimate` WHERE eid ='$eid' ";
+
+	$result = $db->query($query) or die($db->error);
+  
+	if ($result){
+	  if ($result->num_rows == 1){
+	
+		//"UPDATE `estimate` SET `IsAccepted`=1 WHERE eid ='$eid'";
+	  }
+	}
+
+	$query ="UPDATE `estimate` SET `IsAccepted`='$isaccepted' WHERE eid ='$eid'";
+	$result = $db->query($query) or die($db->error);
+   
+  
+	if ($result){
+	 
+	  $result = true;
+	}
+	return $result;
+
+
+  }
+
+
+
+
+ 
+ 
 
  // ------- getter methods ----------
- public function getJObName(){
-   return $this->jobname;
- }
-
- public function getLoc(){
-   return $this->loc;
- }
-
- public function getDescrip(){
-   return $this->descrip;
- }
-
- public function getEcost(){
-   return $this->estcost;
- }
-
- public function getSdate(){
-   return $this->sdate;
- }
- public function getEdate(){
-   return $this->edate;
- }
- public function getCid(){
-   return $this->cid;
- }
- public function getJid(){
-   return $this->jid;
- }
-
-
+ public function get_eid(){
+	return $this->eid;
+  }
+ 
+  public function get_jid(){
+	return $this->jid;
+  }
+ 
+  public function get_tid(){
+	return $this->tid;
+  }
+ 
+  public function get_lcost(){
+	return $this->lcost;
+  }
+ 
+  public function get_tcost(){
+	return $this->tcost;
+  }
+  public function getEdate(){
+	return $this->edate;
+  }
+  public function get_total(){
+	return $this->Total_cost;
+  }
+  public function get_isAccepted(){
+	return $this->isAccepted;
+  }
+ 
+ 
 
 }
 
