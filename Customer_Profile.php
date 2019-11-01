ad3e5aeef4b696e65553af3795652d1488b446cd
Kimster07 <karanmanjrekar.17@gmail.com>
2019-11-02 11:32:47 +1300

    Styling and Bootstrap 4
    Some changes in the styling of these pages and testing bootstrap 4.

--- Customer_Profile.php
+++ Customer_Profile.php
@@ -19,92 +19,93 @@ $uid = $_SESSION['uid'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Customer Dashboard</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-
-    <style>
-
-        .jumbotron{
-            background-color:#2E2D88;
-            color:white;
-        }
-
-
-    </style>
-
-</head>
-<body>
+        <link rel="stylesheet" href="css/style.css">
 
 
 
+    </head>
+    <body>
 
-<!-- .navbar-fixed-top, or .navbar-fixed-bottom can be added to keep the nav bar fixed on the screen -->
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md bg-light navbar-light">
+        <a class="pull-left"><img src="Images/Logo.png" width="60px" height="60px"></a>
+            <a class="navbar-brand js-scroll-trigger" href="Trader_Dash.php">&nbsp Safe Trade</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <div class="navbar-nav ml-auto">
 
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="Create_Job.php">Create Job </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="My_Jobs.php">My Jobs </a>
+            </li>
 
-            <!-- Button that toggles the navbar on and off on small screens -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-
-                <!-- Hides information from screen readers -->
-                <span class="sr-only"></span>
-
-                <!-- Draws 3 bars in navbar button when in small mode -->
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-
-            </button>
-
-            <!-- You'll have to add padding in your image on the top and right of a few pixels (CSS Styling will break the navbar) -->
-            <a class="pull-left"><img src="Images/Logo.png" width="60px" height="60px"></a>
-            <a class="navbar-brand" href="#">&nbsp Safe Trade</a>
-
+            <li class="nav-item">
+                <a class="nav-link" href="Customer_Dash.php">Home </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Customer_LogOut.php">LogOut </a>
+            </li>    
+            </ul>
         </div>
+        </div>  
+        </nav>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div class="navbar-form navbar-right">
-                <ul class="nav navbar-nav">
 
-                    <li><a href="Create_Job.php">Create Job </a></li>
-                    <li><a href="My_Jobs.php">My Jobs </a></li>
-                    <li><a href="Customer_Dash.php">Home </a></li>
-                    <li><a href="Customer_LogOut.php">LogOut </a></li>
-
-                </ul>
-
+        <div align="center">
+        <br><br><br>
+        <div class="row justify-content-center">
+                <div class="col-lg-8 text-center">
+                    <h2 id="h" class="mt-0">Profile</h2>
+                </div>
             </div>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
-<table class="table-condensed">
-<tr>
-  <th>Full Name: </th>
-  <td><?php echo $result->getName();  ?></td>
-</tr>
-<tr>
-  <th>Username: </th>
-  <td><?php echo $result->getUname(); ?></td>
-</tr>
-<tr>
-  <th>Email: </th>
-  <td><?php echo $result->getEmail(); ?></td>
-</tr>
-<tr>
-  <th>Password: </th>
-  <td><?php echo $result->getPass(); ?></td>
-</tr>
+            <div class="container" id="contact">
+                <br><br>
+            
+            <div class="row">
+                <div class="col-lg-3 ml-auto text-center mb-5 mb-lg-0"></div>
+                <div class="col-lg-2 ml-auto text-center mb-5 mb-lg-0">
+                <h5>Trader Name</h5>
+                <div><?php echo $result->getName();  ?></div>
+                </div>
+
+                <div class="col-lg-2 ml-auto text-center mb-5 mb-lg-0">
+                <h5>Username</h5>
+                <div><?php echo $result->getUname(); ?></div>
+                </div>
+
+
+                <div class="col-lg-2 ml-auto text-center mb-5 mb-lg-0">
+                <h5>E-Mail</h5>
+                <div><?php echo $result->getEmail(); ?></div>
+                </div>
+                <div class="col-lg-3 ml-auto text-center mb-5 mb-lg-0"></div>
 
 
 
-</table>
+                </div>
+            </div>
+            </div>
+</div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 </html>
